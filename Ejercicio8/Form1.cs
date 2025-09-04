a5cc36f2ed42c598b4920a5e37e42b447ef337cf
Juan P. Dugo <jdugo@neix.com.ar>
2025-09-04 12:41:12 -0300

    Ejercicio 8

--- Ejercicio8/Form1.cs
+++ Ejercicio8/Form1.cs
@@ -6,5 +6,20 @@ namespace Ejercicio8
         {
             InitializeComponent();
         }
+
+        private string MayusculaRecursivo(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                return "";
+
+            // Procesa el primer carácter y concatena con el resultado recursivo del resto
+            char primerCaracter = char.ToUpper(texto[0]);
+            return primerCaracter + MayusculaRecursivo(texto.Substring(1));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = MayusculaRecursivo(textBox1.Text);
+        }
     }
 }
